5ea28346aad461d2bbf33be4f5a5d1016e966169
abdullah <a.ylcin47@gmail.com>
2023-05-15 14:55:15 +0300

    proje 2 kalsörü oluşturuldu basit şekilde todo list uygulaması yapıdlı

--- proje2/src/App.js
+++ proje2/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+
+
+
+  function addItem() {
+    if (!newItem) {
+      alert("boş veri kayıt edemezsi");
+      return;
+    }
+
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    }
+    setItems(oldItems => [...oldItems, item])
+    setNewItem("")
+  }
+  function deleteItem(id) {
+    const newArray = items.filter(item => item.id !== id)
+    setItems(newArray)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo list proje</h1>
+
+      <input
+        type='text'
+        placeholder='add an items'
+        value={newItem}
+        onChange={e => setNewItem(e.target.value)} />
+
+      <button onClick={() => addItem()}>add</button>
+      <ul>
+        {items.map(item => {
+          return (
+            <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)}>sil</button> </li>
+          )
+        })}
+
+      </ul>
     </div>
   );
 }
