8709fd0b7e5250b81f89364a33f03a86b84deccf
abdullah <a.ylcin47@gmail.com>
2023-05-16 17:28:13 +0300

    hook ile iişlemler yapıldı useefect ve useref ile basit uygulama yapıldı

--- appYazılan.js
+++ appYazılan.js
@@ -51,6 +51,64 @@ function App() {
 }
 
 
+
+//ARKA PLANI KOYU YADA AÇIK YAPMA TEMA RENK DEĞİŞTİRME KODLARI
+
+// import React, { useState } from 'react';
+// import './App.css';
+// import Icerik from './components/icerik';
+
+// export const TemaVerisi = React.createContext();
+
+// const tema = {
+//   dark: {
+//     color: "white",
+//     backgroundColor: "black"
+//   },
+//   light: {
+//     color: "black",
+//     backgroundColor: "white"
+//   }
+// }
+
+// function App() {
+//   const ad = "abdullah47";
+
+//   const [color, setColor] = useState(tema.dark);
+//   function degistir() {
+//     if (tema.dark === color) {
+//       setColor(tema.light)
+//     }
+//     else {
+//       setColor(tema.dark)
+//     }
+//   }
+
+//   return (
+//     <div className='App'>
+//       <button onClick={degistir}>{tema.dark === color ? "açık" : "koyu"} </button>
+//       <TemaVerisi.Provider value={color} >
+
+//         <Icerik />
+
+//       </TemaVerisi.Provider>
+
+//     </div>
+
+//   )
+// }
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
 // function Container(props) {
 //   return (
 //     <></>
--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
-
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react'
 import './App.css';
-import Icerik from './components/icerik';
-
-export const TemaVerisi = React.createContext();
-
-const tema = {
-  dark: {
-    color: "white",
-    backgroundColor: "black"
-  },
-  light: {
-    color: "black",
-    backgroundColor: "white"
-  }
-}
-
 function App() {
-  const ad = "abdullah47";
 
-  const [color, setColor] = useState(tema.dark);
-  function degistir() {
-    if (tema.dark === color) {
-      setColor(tema.light)
-    }
-    else {
-      setColor(tema.dark)
-    }
+
+  // useEffect iele sayaş uygulamsı yapıldı
+  // const [timer, setTimer] = useState(4)
+  // useEffect(() => {
+  //   if (timer !== 0) {
+  //     const interVal = setInterval(() => {
+  //       setTimer(timer - 1)
+  //     }, 1000);
+  //     return () => clearInterval(interVal);
+  //   }
+  //   else {
+  //   }
+  // }, [timer])
+  // // const [count, setCount] = useState(0);
+  // // useEffect(() => {
+  // //   console.log(count);
+  // // },[])
+
+
+  //BURADA USEREF örneği yapıldı
+
+  const [renk, setRenk] = useState("red");
+  const renkDegisimi = useRef(null);
+
+
+  const degisim = () => {
+    document.body.style.backgroundColor = renkDegisimi.current.value;
+    setRenk(() => renkDegisimi.current.value);
   }
+
+
+
 
   return (
     <div className='App'>
-      <button onClick={degistir}>{tema.dark === color ? "açık" : "koyu"} </button>
-      <TemaVerisi.Provider value={color} >
+      {/* <div style={{
+        height: "250px", width: "250px", backgroundColor: "blue", borderRadius: "50%", color: "white", fontSize: "60px", margin: "1opx auto", lineHeight: "250px"
+      }}>{timer} </div> */}
 
-        <Icerik />
-
-      </TemaVerisi.Provider>
-
+      <p>lütfen renklerin ingilizce ismini girin</p>
+      <input type='text' ref={renkDegisimi} onChange={degisim} />
+      <p> {renk} </p>
     </div>
-
   )
 }
-export default App;
+
+export default App
